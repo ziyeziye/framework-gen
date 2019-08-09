ce57017f50ee37b2b856346e049e072b5159205e
ziyeziye <547562496@qq.com>
2019-08-09 10:06:16 +0800

    up

--- template/router.go
+++ template/router.go
@@ -7,16 +7,6 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// example for init the database:
-//
-//  DB, err := gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/employees?charset=utf8&parseTime=true")
-//  if err != nil {
-//  	panic("failed to connect database: " + err.Error())
-//  }
-//  defer db.Close()
-
-var DB *gorm.DB
-
 func ConfigRouter(router *gin.RouterGroup) {
     {{range .}}config{{pluralize .}}Router(router)
     {{end}}
